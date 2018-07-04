cfd7db133005df31f10e41ae3e12a21df5f33980
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-07-04 18:06:09 +0200

    finalize xauthentication

--- api_login/interface.js
+++ api_login/interface.js
@@ -1,79 +1,79 @@
 var nacl;
 var DEBUG = false;
 
-var IoC = function(){
+var IoC = function () {
   var user_keys;
   var assets = {};
   var deterministic = {};
   var initial_session_data;
   var secondary_session_data;
+  var server_session_data;
 
-  this.init =function(callback){
-    nacl_factory.instantiate(function(naclinstance){
+  this.init = function (callback) {
+    nacl_factory.instantiate(function (naclinstance) {
       nacl = naclinstance; // nacl is a global that is initialized here.
-      if(typeof callback === "function"){callback();}
-    }.bind({callback:callback}));
+      if (typeof callback === 'function') { callback(); }
+    });
   };
 
-  this.login = function(username,password){
-    user_keys  = generateKeys(password,username,0);
+  this.login = function (username, password) {
+    user_keys = generateKeys(password, username, 0);
     initial_session_data = generateInitialSessionData(0);
   };
 
-  this.logout = function(){
+  this.logout = function () {
     deterministic = {};
     assets = {};
     user_key = undefined;
     initial_session_data = undefined;
     secondary_session_data = undefined;
-  }
+  };
 
-  this.initAsset = function (assetDetails,deterministicCodeBlob){
-    deterministic[assetDetails["keygen-base"]] = activate(LZString.decompressFromEncodedURIComponent(deterministicCodeBlob));
-    console.log(Object.keys(deterministic[assetDetails["keygen-base"]]));
-
+  this.initAsset = function (assetDetails, deterministicCodeBlob) {
+    deterministic[assetDetails['keygen-base']] = activate(LZString.decompressFromEncodedURIComponent(deterministicCodeBlob));
     assets[assetDetails.symbol] = assetDetails;
     assets[assetDetails.symbol].data = {};
-    assets[assetDetails.symbol].data.seed = seedGenerator(user_keys,assetDetails["keygen-base"]);
-    assets[assetDetails.symbol].data.keys = deterministic[assetDetails["keygen-base"]].keys(assets[assetDetails.symbol].data);
+    assets[assetDetails.symbol].data.seed = seedGenerator(user_keys, assetDetails['keygen-base']);
+    assets[assetDetails.symbol].data.keys = deterministic[assetDetails['keygen-base']].keys(assets[assetDetails.symbol].data);
     assets[assetDetails.symbol].data.keys.mode = assetDetails.mode.split('.')[1]; // (here submode is named mode confusingly enough)
-    assets[assetDetails.symbol].data.address = deterministic[assetDetails["keygen-base"]].address(assets[assetDetails.symbol].data.keys);
+    assets[assetDetails.symbol].data.address = deterministic[assetDetails['keygen-base']].address(assets[assetDetails.symbol].data.keys);
   };
 
-  this.getAddress = function(symbol){
+  this.getAddress = function (symbol) {
     return assets[symbol].data.address;
-  }
-
-  this.xAuthStep0Request = function(){
-    return '/xauth/'+initial_session_data.session_hexkey+'/0';
-  }
-
-  this.xAuthStep1Request = function(nonce1){
-    secondary_session_data = generateSecondarySessionData(nonce1, initial_session_data.session_hexkey, initial_session_data.session_secsign); //TODO
-    var data;//TODO
-    var sessionData; //TODO
-    var cb; //TODO
-
-    var reply = sessionStep1Reply(data, sessionData, cb);
-    //  sess_hex,
-    //  current_nonce
-    return '/xauth/'+initial_session_data.session_hexkey+'/1/'+sess_hex;
-  }
-
-  this.yChanRequest = function(path){
-    var encrypted_path;//TODO;
-    return '/ychan/'+initial_session_data.session_hexkey+'/'+nonce+'/'+encrypted_path;
-  }
-
-  this.zChanRequest = function(path){
-    var encrypted_path;//TODO;
-    return '/zchan/'+initial_session_data.session_hexkey+'/'+nonce+'/'+encrypted_path;
-  }
-
-  this.yChanDecode = function(response){}//TODO
-  this.zChanDecode = function(response){}//TODO
-
-  this.signedTransaction = function(symbol,amount,bla){
-    //return a signed transaction in that can be pushed
   };
-}
+
+  this.xAuthStep0Request = function () {
+    return '/x/' + initial_session_data.session_hexsign + '/0';
+  };
+
+  this.xAuthStep1Request = function (nonce1) {
+    try {
+      secondary_session_data = generateSecondarySessionData(nonce1, initial_session_data.session_hexkey, initial_session_data.session_signpair.signSk);
+    } catch (e) {
+      console.log(JSON.stringify(e));
+    }
+    return '/x/' + initial_session_data.session_hexsign + '/1/' + secondary_session_data.crypt_hex;
+  };
+
+  this.xAuthFinalize = function (data) {
+    server_session_data = data;
+  };
+
+  this.yChanRequest = function (path) {
+    var encrypted_path;// TODO;
+    return '/ychan/' + initial_session_data.session_hexkey + '/' + nonce + '/' + encrypted_path;
+  };
+
+  this.zChanRequest = function (path) {
+    var encrypted_path;// TODO;
+    return '/zchan/' + initial_session_data.session_hexkey + '/' + nonce + '/' + encrypted_path;
+  };
+
+  this.yChanDecode = function (response) {};// TODO
+  this.zChanDecode = function (response) {};// TODO
+
+  this.signedTransaction = function (symbol, amount, bla) {
+    // return a signed transaction in that can be pushed
+  };
+};
--- api_login/login.js
+++ api_login/login.js
@@ -1,40 +1,47 @@
-function go(){
-  function init(callback){
-
-
-    var assetDetails = {"fee":"0.00100000","factor":"8","contract":"undefined","symbol":"waves","name":"Waves","mode":"waves","unified-symbols":"undefined","fee-symbol":"waves","keygen-base":"waves","generated":"never"};
-
-    //{"fee":"0.00002500","factor":"8","contract":"Not yet implemented!","symbol":"btc","name":"Bitcoin","mode":"bitcoinjslib.bitcoin","unified-symbols":"undefined","fee-symbol":"btc","keygen-base":"btc","generated":"never"};
-
-    ioc.login("POMEW4B5XACN3ZCX","TVZS7LODA5CSGP6U");
-
-    var query = 's/deterministic/code/'+assetDetails.mode.split(".")[0];
-
+function go () {
+  function hybriddCall (host, query, callback) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = e => {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        var responseObject = JSON.parse(xhr.responseText);//JSON.parse
-        if(responseObject.hasOwnProperty("id") && responseObject.id === "id"){
-          setTimeout(function(){
-            xhr.open('GET', "http://localhost:1111/p/"+responseObject.data, true);
+        var responseObject = JSON.parse(xhr.responseText);// JSON.parse
+        if (responseObject.hasOwnProperty('id') && responseObject.id === 'id') {
+          setTimeout(() => {
+            xhr.open('GET', 'http://localhost:1111/p/' + responseObject.data, true);
             xhr.send();
-          },1000);
-        }else{
-
-          ioc.initAsset(assetDetails,responseObject.data);
-          //        console.log(ioc.getAddress(assetDetails.symbol));
-          //          console.log(ioc.xAuthStep0());
-          console.log("appeltaart1");
-          console.log(JSON.stringify(ioc.xAuthStep1('5cb2db60e83ec43d0a8390a5332ea49f84c35289f11dc4eb')));
-          console.log("appeltaart2");
+          }, 1000); // TODO fix ugly timeout
+        } else {
+          callback(responseObject);
         }
       }
-    }
+    };
 
-    xhr.open('GET', "http://localhost:1111/"+query, true);
+    xhr.open('GET', host + query, true);
     xhr.send();
+  }
 
+  function init (callback) {
+    var assetDetails = {'fee': '0.00100000', 'factor': '8', 'contract': 'undefined', 'symbol': 'waves', 'name': 'Waves', 'mode': 'waves', 'unified-symbols': 'undefined', 'fee-symbol': 'waves', 'keygen-base': 'waves', 'generated': 'never'};
+
+    // {"fee":"0.00002500","factor":"8","contract":"Not yet implemented!","symbol":"btc","name":"Bitcoin","mode":"bitcoinjslib.bitcoin","unified-symbols":"undefined","fee-symbol":"btc","keygen-base":"btc","generated":"never"};
+
+    ioc.login('POMEW4B5XACN3ZCX', 'TVZS7LODA5CSGP6U');
+
+    var query = 's/deterministic/code/' + assetDetails.mode.split('.')[0];
+
+    hybriddCall('http://localhost:1111/', query, (response) => {
+      ioc.initAsset(assetDetails, response.data);
+      console.log(ioc.getAddress(assetDetails.symbol));
+
+      hybriddCall('http://localhost:1111/', ioc.xAuthStep0Request(), (response) => {
+        var nonce1 = response.nonce1;
+
+        hybriddCall('http://localhost:1111/', ioc.xAuthStep1Request(nonce1), (response) => {
+          ioc.xAuthFinalize(response);
+          console.log('Successfully created a session!');
+        });
+      });
+    });
   }
 
   var ioc = new IoC();
