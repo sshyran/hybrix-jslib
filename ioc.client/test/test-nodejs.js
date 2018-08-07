5eae1686d19be8f6d901a89f2c8911d660935284
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-08-07 17:59:17 +0200

    fix window issue

--- ioc.client/test/test-nodejs.js
+++ ioc.client/test/test-nodejs.js
@@ -6,27 +6,26 @@ function initNACL(callbackArray) {
     functions.sequential(this.callbackArray);
   }.bind({callbackArray: callbackArray})
   );
-}*/
+} */
+window = {};
 
 nacl_factory = require('../../crypto/nacl.js');
- 
+
 var IoC = require('../ioc.nodejs.client.js');
 var ioc = new IoC.Interface({http: require('http')});
 
 ioc.sequential([
-    'init',
-    {username: 'POMEW4B5XACN3ZCX', password: 'TVZS7LODA5CSGP6U'}, 'login',
-    {host: 'http://localhost:1111/'}, 'addHost',
-    {
-      dummy: testAsset('dummy')
-    }, 'parallel'
-  ]
-    , (data)  => { console.log(JSON.stringify(data)); }
-    , (error) => { console.error(error); }
-  );
+  'init',
+  {username: 'POMEW4B5XACN3ZCX', password: 'TVZS7LODA5CSGP6U'}, 'login',
+  {host: 'http://localhost:1111/'}, 'addHost',
+  {
+    dummy: testAsset('dummy')
+  }, 'parallel'
+]
+  , (data) => { console.log(JSON.stringify(data)); }
+  , (error) => { console.error(error); }
+);
 
-
-   
 function testAsset (symbol) {
   var testAmount = 0.0001;
 
