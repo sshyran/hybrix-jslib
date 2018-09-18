881a354c92bb81b6bfc5b72ea74a852b3fbf5549
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-09-18 17:44:04 +0200

    fix plain text storage

--- examples/dummy.js
+++ examples/dummy.js
@@ -1,13 +1,15 @@
 nacl_factory = require('../common/crypto/nacl.js');
 
-var Hybridd = require('..dist/hybridd.interface.nodejs.js');
-var hybridd = new Hybridd.Interface({http: require('http')});
+var Hybridd = require('../dist/hybridd.interface.nodejs.js');
+var hybridd = new Hybridd.Interface({http: require('https')});
 
 hybridd.sequential([
   'init',
-  {username: '*****', password: '****'}, 'login',
+    {username: '***', password: '***'}, 'session',
   {host: 'http://localhost:1111/'}, 'addHost',
-  {symbol: 'dummy', amount: 100, channel: 'y'}, 'rawTransaction'
+
+  {key:"appel",value:"taart",encrypted:true}, 'save'
+//  {symbol: 'dummy', amount: 100, channel: 'y'}, 'rawTransaction'
 ]
   , (data) => { console.log(JSON.stringify(data)); }
   , (error) => { console.error(error); }
