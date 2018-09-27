9237609fedbb7340ccdc280206f958b5f2c835e6
Wouter Uijens <w.r.uijens@gmail.com>
2018-09-27 16:09:03 +0200

    Example for adding orders finally works. Waves is really strange in that it imposes an ordering on its assets which needs to be used correctly when making an order. In communicating with the waves devs, they didn't give a way to consistently find this ordering, so a heuristic was used (which works for example for any transaction between WAVES itself and a WAVES token). The ugly hack used now is to try the heuristic and determine whether it was correct. If not a parameter is added to swap the assets, which will then always work. This means the example contains some ugly code to make sure both cases (heuristic succeeding/failing) are accounted for. The current code should always work then.

--- examples/example-waves-dex-send-order.js
+++ examples/example-waves-dex-send-order.js
@@ -4,15 +4,15 @@ var Hybridd = require('../dist/hybridd.interface.nodejs.js');
 var hybridd = new Hybridd.Interface({http: require('http')});
 
 // IoC credentials:
-var username = 'I4X4KG67OBET5TS5';
-var password = 'HD6L5K4JEOM2PZNE';
+var username = '';
+var password = '';
 
 // Parameters of the order
-var sendAsset = 'waves.vtn';
-var sendAmount = "1";
-var receiveAsset = 'waves.wct';
-var receiveAmount = "100";
-var maxLifetimeInSeconds = 3600
+var sendAsset = 'waves';
+var sendAmount = "0.01";
+var receiveAsset = 'waves.usd';
+var receiveAmount = "0.1";
+var maxLifetimeInSeconds = 3600 // maximum lifetime is one month
 var matcherFee = 300000 // default matcher fee found in python implementation: https://github.com/PyWaves/PyWaves/blob/master/__init__.py#L16
 
 var host = 'http://localhost:1111/';
@@ -38,14 +38,45 @@ hybridd.sequential([
     privKey:   {data: result.keyPair.privateKey, step: 'id'}}
   }, 'parallel',
   (result) => {
-    return {data: result, id: 'dex_waves', func: 'makeSignedWavesOrder'};
-  }, 'client',
+    return {
+      parameters: {data: result, step: 'id'},
+      signedTransaction: {data: {data: result, id: 'dex_waves', func: 'makeSignedWavesOrder'}, step: 'client'}
+      };
+  }, 'parallel',
   (result) => {
-    console.log(result)
-    return {query: '/engine/dex_waves/push/' + JSON.stringify(result)};
+    return {
+      response: {data: {query: '/engine/dex_waves/push/' + JSON.stringify(result.signedTransaction)}, step: 'call'},
+      parameters: {data: result.parameters, step: 'id'}
+    };
   },
-  'call'
+  'parallel',
+  (result) => {
+    if( result.response.hasOwnProperty('message') && result.response.message == 'Pair should be reverse' ) {
+      parametersSwappedAssets = result.parameters
+      parametersSwappedAssets.swap = true;
+      return {
+        signedTransaction: {data: {data: parametersSwappedAssets, id: 'dex_waves', func: 'makeSignedWavesOrder'}, step: 'client'}
+      };
+    } else {
+      return {
+        response: {data: result.response, step: 'id'}
+      };
+    }
+  },
+  'parallel',
+  (result) => {
+    if(result.hasOwnProperty('signedTransaction')) {
+      return {
+        response: {data: {query: '/engine/dex_waves/push/' + JSON.stringify(result.signedTransaction)}, step: 'call'}
+      };
+    } else {
+      return {
+        response: {data: result.response, step: 'id'}
+      };
+    }
+  },
+  'parallel'
 ]
-  , (data) => { console.log(data); }
+  , (data) => { console.log(data.response); }
   , (error) => { console.error(error); }
 );
