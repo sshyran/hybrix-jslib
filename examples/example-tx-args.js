e91b8c4fbd224700306e3ecd5759f32efceca930
agent725 <agent725@725.be>
2018-10-11 14:42:33 +0200

    command line argument driven example updated

--- examples/example-tx-args.js
+++ examples/example-tx-args.js
@@ -1,9 +1,22 @@
 nacl_factory = require('../common/crypto/nacl.js');
 
-var Hybridd = require('..dist/hybridd.interface.nodejs.js');
+//var Hybridd = require('../dist/hybridd.interface.nodejs.js');
+var Hybridd = require('../lib/interface.js');
 var hybridd = new Hybridd.Interface({http: require('http')});
+var ProgressBar = require('progress');
+var stdio = require('stdio');
 
-var hostname = 'http://wallet-uat.internetofcoins.org/api/';
+function makeProgressBar(title) {
+  bar = new ProgressBar(' [.] '+title+': [:bar] :percent, eta: :etas', {
+    complete: '▓',
+    incomplete: '░',
+    width: 76-title.length,
+    total: 100
+  });  
+}
+
+//var hostname = 'http://wallet-uat.internetofcoins.org/api/';
+var hostname = 'http://127.0.0.1:1111/';
 
 // command line options and init
 var stdio = require('stdio');
@@ -11,10 +24,10 @@ var ops = stdio.getopt({
   'userid': {key: 'u', args: 1, description: 'Set username'},
   'passwd': {key: 'p', args: 1, description: 'Set password'},
   'newaccount': {key: 'n', description: 'Generate new wallet'},
-  'sendtransaction': {key: 't', args: 3, description: 'Send transaction [argument: symbol] [argument: amount] [argument: target_address]'},
-  'addasset': {key: 'a', args: 1, description: 'Add asset to wallet [argument: symbol]'},
-  'addassets': {key: 'A', args: 1, description: 'Add assets to wallet [argument: symbol1,symbol2,symbol3]'},
   'getaddress': {key: 'g', args: 1, description: 'Add asset to wallet [argument: symbol]'},
+  'sendtransaction': {key: 't', args: 3, description: 'Send transaction [argument: symbol] [argument: amount] [argument: target_address]'},
+  // TODO: 'addasset': {key: 'a', args: 1, description: 'Add asset to wallet [argument: symbol]'},
+  // TODO: 'addassets': {key: 'A', args: 1, description: 'Add assets to wallet [argument: symbol1,symbol2,symbol3]'},
   'pubkey': {key: 'P', args: 1, description: 'Get public key from wallet [argument: symbol]'},
   'privkey': {key: 'S', args: 1, description: 'Get private key from wallet [argument: symbol]'},
   'keypair': {key: 'K', args: 1, description: 'Get public and private key from wallet [argument: symbol]'},
@@ -39,18 +52,45 @@ if(base === 'xcp' || base === 'omni') {
   base = 'btc';
 } */
 
+var progress = [];
+
+var actions = [
+  'init',
+  {username: userid, password: passwd}, 'session',
+  {host: hostname}, 'addHost'
+];
+
+if (ops.getaddress) {
+  progress.push('getaddress');
+  actions.push({symbol:ops.getaddress});
+  actions.push('addAsset');
+  actions.push({symbol:ops.getaddress});
+  actions.push('getAddress');
+}
+
+if (ops.pubkey) {
+  progress.push('pubkey');
+  actions.push({symbol:ops.pubkey});
+  actions.push('addAsset');
+  actions.push({symbol:ops.pubkey});
+  actions.push('getPublicKey');
+}
+
 if (ops.rawtransaction) {
+  progress.push('rawtx');
   var amount = ops.rawtransaction[1];
   var target = ops.rawtransaction[2];
-
-  hybridd.sequential([
-    'init',
-    {username: userid, password: passwd}, 'session',
-    {host: hostname}, 'addHost',
-    {symbol: symbol, amount: Number(amount), target: target }, 'rawTransaction'
-  ]
-    , (data) => { console.log(data + '\n'); }
-    , (error) => { console.error('Error: ' + error); }
-    , ops.quiet ? undefined : (progress) => { console.log(Math.floor(progress * 100) + '%'); }
-  );
+  actions.push({symbol:symbol, amount:Number(amount), target:target });
+  actions.push('rawTransaction');
 }
+
+// show progress
+makeProgressBar(progress.join(','));
+
+// take action
+hybridd.sequential( 
+  actions
+  , (data) => { if(!ops.quiet) { bar.update(1); } console.log(data + "\n"); }
+  , (error) => { console.error("\n"+'Error: ' + error); }
+  , ops.quiet ? undefined : (progress) => { if(!ops.quiet) { bar.update(progress); } }
+);
