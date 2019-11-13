c189893f32e543e26b5190bc566522c52c1ccdb0
“Marco <marco.alkema@gmail.com>
2019-11-13 13:11:04 +0100

    #14-Add sleep.

--- scripts/npm/publish.sh
+++ scripts/npm/publish.sh
@@ -1,11 +1,23 @@
+# Publish on NPM
+
 echo "Publishing to NPM"
 
-# Publish on NPM
+# Get correct version from repository tag
+version=${CI_COMMIT_REF_NAME/-release/}
+versionNumber="${version//v}"
+
+sed -i -e "s/.*version.*/\"version\": \"$versionNumber\",/" package.json
+
 npm install npm-cli-login
-node ./node_modules/npm-cli-login/bin/npm-cli-login.js -u "$NPM_USER" -p "$NPM_PASSWORD" -e "$NPM_MAIL"
+node ./node_modules/npm-cli-login/bin/npm-cli-login.js -u "$NPM_USER" -p "$NPM_PASSWORD" -e "$NPM_MAIL" -r "https://registry.npmjs.org"
+sleep 20s
 npm publish
 
 # Publish on Github
+touch .npmrc
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_PAT" >> .npmrc
+echo "@hybrix-io:registry=https://npm.pkg.github.com" >> .npmrc
+
 sed -i -e "\$i \,\"publishConfig\": { \"registry\": \"https://npm.pkg.github.com/\" }" package.json
-node ./node_modules/npm-cli-login/bin/npm-cli-login.js -u "$GITHUB_USER" -p "$GITHUB_PASSWORD" -e "$GITHUB_MAIL" -r "https://npm.pkg.github.com/"
+
 npm publish
