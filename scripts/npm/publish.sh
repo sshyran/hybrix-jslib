31c36033eff8416683683ad123457c644edaa241
“Marco <marco.alkema@gmail.com>
2019-11-13 13:46:39 +0100

    #14-Update publish script.

--- scripts/npm/publish.sh
+++ scripts/npm/publish.sh
@@ -1,12 +1,11 @@
 # Publish on NPM
+# Get correct version from repository tag
+VERSION=${CI_COMMIT_REF_NAME/-release/}
+VERSION_NUMBER="${VERSION//v}"
 
 echo "Publishing to NPM"
 
-# Get correct version from repository tag
-version=${CI_COMMIT_REF_NAME/-release/}
-versionNumber="${version//v}"
-
-sed -i -e "s/.*version.*/\"version\": \"$versionNumber\",/" package.json
+sed -i -e "s/.*version.*/\"version\": \"$VERSION_NUMBER\",/" package.json
 
 npm install npm-cli-login
 node ./node_modules/npm-cli-login/bin/npm-cli-login.js -u "$NPM_USER" -p "$NPM_PASSWORD" -e "$NPM_MAIL" -r "https://registry.npmjs.org"
