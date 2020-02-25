27c1921546eafd625cbb0e05519cb55d090f94c3
Rouke Pouw <roukepouw@gmail.com>
2020-02-25 12:37:10 +0000

    Issue/47 setup script update for checking folders

--- scripts/npm/setup.sh
+++ scripts/npm/setup.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 OLDPATH=$PATH
 WHEREAMI=`pwd`
-NODEINST=`which node`
 
 # $HYBRIXD/interface/scripts/npm  => $HYBRIXD
 SCRIPTDIR="`dirname \"$0\"`"
 HYBRIXD="`cd \"$SCRIPTDIR/../../..\" && pwd`"
 
-NODE="$HYBRIXD/node"
-DETERMINISTIC="$HYBRIXD/deterministic"
 NODEJS="$HYBRIXD/nodejs"
 COMMON="$HYBRIXD/common"
-WEB_WALLET="$HYBRIXD/web-wallet"
-ENVIRONMENT="$1"
 
-if [ "$ENVIRONMENT" = "dev" ]; then
-    URL_COMMON="https://gitlab.com/hybrix/hybrixd/common.git"
-    URL_NODEJS="https://www.gitlab.com/hybrix/hybrixd/dependencies/nodejs.git"
-    INTERFACE="$HYBRIXD/interface"
-    echo "[i] Environment is development..."
-elif [ "$ENVIRONMENT" = "public" ]; then
+if [  -e "$HYBRIXD/hybrix-jslib"  ]; then
     URL_COMMON="https://github.com/hybrix-io/common.git"
     URL_NODEJS="https://github.com/hybrix-io/nodejs.git"
+    URL_NODE="https://github.com/hybrix-io/hybrixd.git"
+    NODE="$HYBRIXD/hybrixd"
     INTERFACE="$HYBRIXD/hybrix-jslib"
+    ENVIRONMENT="public"
     echo "[i] Environment is public..."
+elif [  -e "$HYBRIXD/interface" ]; then
+    URL_COMMON="https://gitlab.com/hybrix/hybrixd/common.git"
+    URL_NODEJS="https://www.gitlab.com/hybrix/hybrixd/dependencies/nodejs.git"
+    URL_NODE="https://www.gitlab.com/hybrix/hybrixd/node.git"
+    INTERFACE="$HYBRIXD/interface"
+    NODE="$HYBRIXD/node"
+    ENVIRONMENT="dev"
+    echo "[i] Environment is development..."
 else
-    echo "[!] Unknown Environment (please use npm run setup[:dev])"
+    echo "[!] Unknown Environment"
     export PATH="$OLDPATH"
     cd "$WHEREAMI"
     exit 1
@@ -33,26 +34,36 @@ fi
 
 if [ "`uname`" = "Darwin" ]; then
     SYSTEM="darwin-x64"
-elif [ "`uname -m`" = "i386" ] || [ "`uname -m`" = "i686" ]; then
-    SYSTEM="x86"
-elif [ "`uname -m`" = "x86_64" ]; then
-    SYSTEM="x86_64"
+elif [ "`uname -m`" = "i386" ] || [ "`uname -m`" = "i686" ] || [ "`uname -m`" = "x86_64" ]; then
+    SYSTEM="linux-x64"
 else
     echo "[!] Unknown Architecture (or incomplete implementation)"
-#    exit 1;
+    export PATH="$OLDPATH"
+    cd "$WHEREAMI"
+    exit 1;
 fi
 
-# NODE
+# INTERFACE
+if [ "$ENVIRONMENT" = "public" ]; then
+    if [ ! -e "$HYBRIXD/interface" ];then
+        echo "[i] linking hybrix-libjs to interface folder."
+        ln -sf "$HYBRIXD/hybrix-libjs" "$HYBRIXD/interface"
+    else
+        echo "[i] interface folder exists."
+    fi
+fi
+
+# NODE_BINARIES
 if [ ! -e "$INTERFACE/node_binaries" ];then
 
-    echo " [!] node_binaries not found."
+    echo "[!] node_binaries not found."
 
     if [ ! -e "$NODEJS" ];then
         cd "$HYBRIXD"
-        echo " [i] Clone node js runtimes files"
+        echo "[i] Clone node js runtimes files"
         git clone "$URL_NODEJS"
     fi
-    echo " [i] Link NODEJS files"
+    echo "[i] Link NODEJS files"
     ln -sf "$NODEJS/$SYSTEM" "$INTERFACE/node_binaries"
 fi
 export PATH="$INTERFACE/node_binaries/bin:$PATH"
@@ -61,21 +72,33 @@ export PATH="$INTERFACE/node_binaries/bin:$PATH"
 # COMMON
 if [ ! -e "$INTERFACE/common" ];then
 
-    echo " [!] common not found."
+    echo "[!] common not found."
 
     if [ ! -e "$COMMON" ];then
         cd "$HYBRIXD"
-        echo " [i] Clone common files"
+        echo "[i] Clone common files"
         git clone "$URL_COMMON"
 
     fi
-    echo " [i] Link common files"
+    echo "[i] Link common files"
     ln -sf "$COMMON" "$INTERFACE/common"
 
 fi
 
 if [ -e "$HYBRIXD/project-xhy" ]; then
     ln -sf "$HYBRIXD/project-xhy" "$INTERFACE/lib/methods/rawTransaction/project-xhy"
+fi
+
+# NODE
+if [ ! -e "$HYBRIXD/node" ];then
+    echo "[!] node not found."
+    cd "$HYBRIXD"
+    echo "[i] Clone node files"
+    git clone "$URL_NODE"
+    ln -sf "hybrixd" "node"
+
+    echo "[i] Run node setup"
+    sh "$NODE/scripts/npm/setup.sh"
 fi
 
 # GIT HOOKS
